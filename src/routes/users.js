9ee36b19e3a767d7c06811eb59a9b52a3f50bc20
Ignacio Cunial <igsecu@hotmail.com>
2023-08-24 16:12:55 -0300

    Created GET /:id route -> get user by id

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,28 @@ const router = express.Router();
 
 const User = require("../models/User");
 
+// Get user by ID
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  let result = [];
+
+  try {
+    result = await usersController.findUserByID(id);
+    if (!result.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `User with ID: ${id} not found!`,
+      });
+    }
+    res.status(200).json({
+      statusCode: 200,
+      data: result,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 // Get All Users
 router.get("/", async (req, res, next) => {
   try {
