2c0c830004d2b04222ad3bef35c3e6dfa761604e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-24 16:05:53 -0300

    Created GET / route -> get all users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,4 +3,23 @@ const router = express.Router();
 
 const User = require("../models/User");
 
+// Get All Users
+router.get("/", async (req, res, next) => {
+  try {
+    const dbResults = await usersController.getAllDb();
+    if (!dbResults.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `There are not users saved in the DB!`,
+      });
+    }
+    res.status(200).json({
+      statusCode: 200,
+      data: dbResults,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 module.exports = router;
