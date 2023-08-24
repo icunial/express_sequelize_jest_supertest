4f912e413edc302b0acb4030ac3a2150bce5f23b
Ignacio Cunial <igsecu@hotmail.com>
2023-08-24 16:02:03 -0300

    Created routes folder / Created /src/routes/index.js file / Created usersRouter -> /src/routes/users.js / Added router middleware -> index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const router = require("./src/routes/index");
+
 // Body-Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Router middleware
+app.use("/", router);
 
 // Initialized Express Server
 app.listen(PORT, () => {
