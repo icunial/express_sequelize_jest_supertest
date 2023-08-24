3767030f9d849b8d3270d44238f86ff71624db17
Ignacio Cunial <igsecu@hotmail.com>
2023-08-24 17:11:34 -0300

    Updated users router -> src/routes/users.js file -> Imported usersController

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User");
+const usersController = require("../controllers/users");
 
 const validations = require("../utils/validations");
 
